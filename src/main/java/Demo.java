f98b6bbb4ef9d2bad49173ea5899608f1528e014
nchitti <nchitti@paypal.com>
2020-03-30 17:40:39 +0530

    Added a Demo.java file

---
+++ src/main/java/Demo.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+        List<String> listStrings = new ArrayList<>();
+        listStrings.add("Five");
+        listStrings.add("Six");
+        listStrings.add("Seven");
+        listStrings.add("Eight");
+
+        if (listStrings.remove("Ten")){
+            System.out.println("removed");
+        } else {
+            System.out.println("there is no such element");
+        }
+    }
+}
